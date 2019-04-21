26a92084646a531fb46ccb211632fa9297d8f254
Oded Zadok <OdedZadok@Hotmail.com>
2019-04-21 22:30:09 +0300

    use http-proxy-middleware

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # MultiTenantRouter
 Node.js router server for support multi tenant  #Express #Node.js #Router
 
-* GIT test
+To develop, we run:
+npm run watch-ts
+
+and in a separate terminal we run:
+npm run watch-node
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,61 @@
 import express from 'express';
+import http from 'http';
+import proxy from 'http-proxy-middleware';
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/Test/api', (req, res) => {
+  res.send('Work' + req.url);
+});
+
+function onProxyServerError(err: Error, req: http.IncomingMessage, res: http.ServerResponse): void {
+  console.error(err);
+}
+
+function onProxyServerReq(proxyReq: http.ClientRequest, req: http.IncomingMessage, res: http.ServerResponse): void {
+  // add custom header to request
+  // proxyReq.setHeader('x-added', 'foobar');
+  // or log the req
+  console.trace(proxyReq);
+}
+
+// proxy middleware options
+const options: proxy.Config = {
+  target: 'http://localhost:35636', // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+
+  pathRewrite: {
+    //   '^/api/old-path': '/api/new-path', // rewrite path
+    //   '^/api/remove/path': '/path' // remove base path
+  },
+  router: {
+    'localhost:3000/api'         : 'http://localhost:35636'  // host + path
+    // when request.headers.host == 'dev.localhost:3000',
+    // override target 'http://www.example.org' to 'http://localhost:8000'
+    //'dev.localhost:3000': 'http://localhost:35636',
+  },
+  onError: (err, req, res) => onProxyServerError(err, req, res),
+  onProxyReq: (proxyReq, req, res) => onProxyServerReq(proxyReq, req, res),
+  logLevel: 'debug',
+};
+
+// create the proxy (without context)
+const exampleProxy = proxy(options);
+
+// mount `exampleProxy` in web server
+app.use('/api', exampleProxy);
+
+// Listen for the `error` event on `proxy`.
+
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+// app.use('/Router/', (req, res, next) => {
+//     console.log(req.url);
+//     res.send("Hello " + req.method + req.url);
+//     next();
+
+// });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-// test
+
